763371b51d9eb47f5cfd2fc23508bfca896b913d
Shridhara Ramaswamy <103982839+ShridharaRamaswamy@users.noreply.github.com>
2023-06-01 23:09:22 +0530

    Add files via upload

--- cypress.config.js
+++ cypress.config.js
@@ -22,27 +22,42 @@ module.exports = defineConfig({
 
       require("cypress-localstorage-commands/plugin")(on, config);
       require("cypress-mochawesome-reporter/plugin")(on, config);
+      require('@cypress/grep/src/plugin')(config);
+
 
       return config;
     },
   },
+  scrollBehavior: 'nearest',
   requestTimeout: 15000,
   defaultCommandTimeout: 60000,
-  pageLoadTimeout: 450000,
+  pageLoadTimeout: 650000,
   video: false,
 
   chromeWebSecurity: false,
 
   env: {
     // url: "http://cloud.msa2.apps.yokogawa.build/eln-webapp/elndev/",
-    url: "http://cloud.msa2.apps.yokogawa.build/eln-webapp-test/elndev/",
+    // url: "http://cloud.msa2.apps.yokogawa.build/eln-webapp-test/elndev/",
+    // url: "http://cloud.msa2.apps.yokogawa.build/eln-webapp-ti/elndev/",
+    url: "http://cloud.msa2.apps.yokogawa.build/eln-webapp-demo/elndev/",
+    // url: "http://cloud.msa2.apps.yokogawa.build/eln-webapp-nfr/elndev/",
     email: "shridhara.ramaswamy@yokogawa.com",
     password: "June@2022",
+    email1: "shridhara055@gmail.com",
+    password1: "June@2022",
+    email2: "panchami.rajanna@yokogawa.com",
+    password2: "Sept@2022",
+    hideXhr: true
+  },
+
+  compilerOptions: {
+    types: ["cypress", "@4tw/cypress-drag-drop"]
   },
 
   retries: {
     // Configure retry attempts for `cypress run`
     // Default is 0
-    runMode: 1,
+    runMode: 0,
   },
 });
